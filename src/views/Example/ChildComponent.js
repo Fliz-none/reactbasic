5121bb45f4e1cb173b27d614e48ad3513e686cbd
LeHaiDang <lhd4388@gmail.com>
2024-11-11 14:13:37 +0700

    scss

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,45 +1,49 @@
-import React from 'react';
-import './Demo.css'
+import React from "react";
+import "./Demo.scss";
 
 class ChildComponent extends React.Component {
+  state = {
+    showJob: false
+  };
 
-    state = {
-        showJob: false
-    }
+  handleShowHide = () => {
+    this.setState({
+      showJob: !this.state.showJob
+    });
+  };
 
-    handleShowHide = () => {
-        this.setState({
-            showJob: !this.state.showJob
-        })
-    }
+  handleRemoveJob = (job) => {
+    this.props.removeJob(job);
+  };
 
-    handleRemoveJob = (job) => {
-        this.props.removeJob(job)
-    }
-
-    render() {
-        let arrJobs = this.props.arrJobs;
-        let showJob = this.state.showJob;
-        return (
-            <div>
-                {showJob ?
-                    <button 
-                        className='btn-show'
-                        onClick={() => this.handleShowHide()}>Show list</button>
-                    :
-                    <>
-                        <button onClick={() => this.handleShowHide()}>Hide list</button>
-                        <div className='job-list'>
-                            {arrJobs.map((job, i) => {
-                                return (
-                                    <div key={job.id}>{job.name} - {job.salary}$ <span onClick={() => this.handleRemoveJob(job)}> x</span></div>
-                                )
-                            })}
-                        </div>
-                    </>
-                }
-            </div >
-        )
-    }
-};
+  render() {
+    let arrJobs = this.props.arrJobs;
+    let showJob = this.state.showJob;
+    return (
+      <div>
+        {showJob ? (
+          <button className="btn-show" onClick={() => this.handleShowHide()}>
+            Show list
+          </button>
+        ) : (
+          <>
+            <button className="btn-hide" onClick={() => this.handleShowHide()}>
+              Hide list
+            </button>
+            <div className="job-list">
+              {arrJobs.map((job, i) => {
+                return (
+                  <div key={job.id}>
+                    {job.name} - {job.salary}${" "}
+                    <span onClick={() => this.handleRemoveJob(job)}> x</span>
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
+}
 export default ChildComponent;
--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,49 +1,55 @@
-import React from 'react';
-import AddComponent from './AddComponent';
-import ChildComponent from './ChildComponent';
+import React from "react";
+import AddComponent from "./AddComponent";
+import ChildComponent from "./ChildComponent";
 
 class MyComponent extends React.Component {
+  state = {
+    arrJobs: [
+      { id: "job02.0", name: "Lê Hải Đăng", salary: "2000" },
+      { id: "job02.5", name: "Đăng Lee", salary: "3000" },
+      { id: "job03.0", name: "Lê Hẻ Đen", salary: "4000" },
+    ]
+  };
+  removeJob = (job) => {
+    let newJob = this.state.arrJobs.filter((item) => item.id != job.id);
+    this.setState({
+      arrJobs: newJob
+    });
+  };
 
-    state = {
-        arrJobs: [
-            { id: 'job01', name: 'Lee Dang', salary: '3000' },
-            { id: 'job02', name: 'Lê Hải Đăng', salary: '2000' },
-            { id: 'job03', name: 'Lê Hẻ Đen', salary: '4000' },
-            { id: 'job04', name: 'LÊ HẢI ĐĂNG', salary: '4000' },
-            { id: 'job05', name: 'LÊ ĐĂNG', salary: '300' },
-        ]
-    }
-    removeJob = (job) => {
-        let newJob = this.state.arrJobs.filter((item) => item.id != job.id);
-        this.setState({
-            arrJobs: newJob
-        })
-    }
+  componentDidMount() {
+    //Định nghĩa dữ liệu ví dụ get API
+    console.log("Did Mount");
+  }
+  componentDidUpdate(prevProps, prevState) {
+    // Sau khi cập nhật state, props thứ tự tham số phải đúng
+    console.log(
+      "Did update, prevstate: ",
+      prevState,
+      " props:",
+      prevProps,
+      " current:",
+      this.state
+    );
+  }
 
-    componentDidMount() {
-        //Định nghĩa dữ liệu ví dụ get API
-        console.log('Did Mount');
-    }
-    componentDidUpdate(prevProps, prevState) {
-        // Sau khi cập nhật state, props thứ tự tham số phải đúng
-        console.log('Did update, prevstate: ', prevState, ' props:', prevProps, ' current:', this.state);
-    }
+  addJob = (job) => {
+    this.setState({
+      arrJobs: [...this.state.arrJobs, job]
+    });
+  };
 
-
-    addJob = (job) => {
-        this.setState({
-            arrJobs: [...this.state.arrJobs, job]
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <AddComponent addJob={this.addJob} />
-                <ChildComponent arrJobs={this.state.arrJobs} removeJob={this.removeJob} />
-            </div>
-        )
-    }
-};
+  render() {
+    return (
+      <div>
+        <AddComponent addJob={this.addJob} />
+        <ChildComponent
+          arrJobs={this.state.arrJobs}
+          removeJob={this.removeJob}
+        />
+      </div>
+    );
+  }
+}
 
 export default MyComponent;
